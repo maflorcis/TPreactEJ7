b3617b5f0b83be16b668440ab40f57c03398e7f2
floralonso <floralonso@gmail.com>
2022-10-02 21:22:40 -0300

    ajusté avatars

--- src/components/Persons.jsx
+++ src/components/Persons.jsx
@@ -9,21 +9,21 @@ const Persons = () => {
       name: "Laya Dueñas",
       role: "CEO",
       sector: "Business",
-      img: "https://bootdey.com/img/Content/avatar/avatar2.png",
+      img: "https://bootdey.com/img/Content/avatar/avatar1.png",
     },
     {
       id: 2,
       name: "Astryd Vallés",
       role: "CMO",
       sector: "Marketing",
-      img: "https://bootdey.com/img/Content/avatar/avatar3.png",
+      img: "https://bootdey.com/img/Content/avatar/avatar2.png",
     },
     {
       id: 3,
       name: "Shantell Meza",
       role: "CFO",
       sector: "Business",
-      img: "https://bootdey.com/img/Content/avatar/avatar4.png",
+      img: "https://bootdey.com/img/Content/avatar/avatar3.png",
     },
 
     {
@@ -39,7 +39,7 @@ const Persons = () => {
         name: "Ares Jiménez",
         role: "Art Director",
         sector: "Marketing",
-        img: "https://bootdey.com/img/Content/avatar/avatar4.png",
+        img: "https://bootdey.com/img/Content/avatar/avatar5.png",
       },
 
       {
@@ -47,7 +47,7 @@ const Persons = () => {
         name: "Marta Perez",
         role: "Frontend Dev",
         sector: "Engineering",
-        img: "https://bootdey.com/img/Content/avatar/avatar4.png",
+        img: "https://bootdey.com/img/Content/avatar/avatar6.png",
       },
 
       {
@@ -55,21 +55,21 @@ const Persons = () => {
         name: "Ellen Balderas",
         role: "Digital Strategist",
         sector: "Marketing",
-        img: "https://bootdey.com/img/Content/avatar/avatar4.png",
+        img: "https://bootdey.com/img/Content/avatar/avatar7.png",
       },
       {
         id: 8,
         name: "Cynthia Valentín",
         role: "Backend Dev",
         sector: "Engineering",
-        img: "https://bootdey.com/img/Content/avatar/avatar4.png",
+        img: "https://bootdey.com/img/Content/avatar/avatar8.png",
       },
       {
         id: 9,
         name: "Bernard Jung",
         role: "Devops Engineer",
         sector: "Engineering",
-        img: "https://bootdey.com/img/Content/avatar/avatar4.png",
+        img: "https://bootdey.com/img/Content/avatar/avatar1.png",
       },
   ]);
 
