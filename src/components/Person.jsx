df12fe02ef947825a27a0268eb6e8d508a175b10
floralonso <floralonso@gmail.com>
2022-10-04 06:06:14 -0300

    ajuste el badge

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -2,20 +2,20 @@ import React from "react";
 
 const Person = (props) => {
   return (
-    <div class="col-lg-12">
-      <div class="card-box">
-        <div class="member-card pt-2 pb-2">
-          <div class="thumb-lg member-thumb mx-auto">
+    <div className="col-lg-12">
+      <div className="card-box">
+        <div className="member-card pt-2 pb-2">
+          <div className="thumb-lg member-thumb mx-auto">
             <img
               src={props.img}
-              class="rounded-circle img-thumbnail"
+              className="rounded-circle img-thumbnail"
               alt="profile-image"
             />
           </div>
           <div class="">
             <h4>{props.name}</h4>
             <p class="text-muted">{props.role}</p>
-           <p class="text-muted">{props.sector}</p>
+           <p className="text-white badge bg-primary">{props.sector}</p>
             
           </div>
         </div>
